79c1c21b6177f2661d9390fb8065e7211e7d8229
tessamcdonnel <tmcdonne@calpoly.edu>
2017-07-16 14:36:33 -0700

    added packages

---
+++ requirements.R
@@ -0,0 +1,6 @@
+devtools::install_version("randomForest", "4.6-12")
+devtools::install_version("ggplot2", "2.2.0")
+devtools::install_version("googleVis", "0.6.1")
+devtools::install_version("caret", "6.0-73")
+devtools::install_version("e1071", "1.6-7")
+devtools::install_version("mosaic", "0.4.14")
