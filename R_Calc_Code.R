7575ca72c0c671e1eabfdd58717d5fe67fc369eb
tessamcdonnel <tmcdonne@calpoly.edu>
2017-07-23 13:57:53 -0700

    added to lab 6 notes

--- R_Calc_Code.R
+++ R_Calc_Code.R
@@ -19,6 +19,16 @@ plot(-6:6, -6:6, type = "n", xlab="", ylab="", xaxt = 'n', yaxt = 'n')
 text(0, 3, expression(paste("Ho:", mu, "=5")), cex = 3)
 text(0, -3, expression(paste("Ha:", mu, ">5")), cex = 3)
 
-plot(1:10, 1:10, type = "n") 
-text(4, 7, expression("This is a subscript "[2]))
+plot(-6:6, -6:6, type = "n", xlab="", ylab="", xaxt = 'n', yaxt = 'n', frame=F)
+text(0, 3, expression(H[o]: mu == mu[1] == mu[2]), cex = 3)
+text(0, -3, expression(H[o]: mu != 5), cex = 3)
+
+
+plot(-6:6, -6:6, type = "n", xlab="", ylab="", xaxt = 'n', yaxt = 'n', frame=F)
+text(0, 4, expression(H[o]: mu[Cont] == mu[CBT] == mu[FT]), cex = 2)
+text(0, 1, expression(paste(H[a]: "at least one mean differs")), cex = 2)
+
+plot(-6:6, -6:6, type = "n", xlab="", ylab="", xaxt = 'n', yaxt = 'n', frame=F)
+text(0, 4, expression(H[o]: mu[Cont] = mu[CBT]), cex = 2)
+text(0, 1, expression(paste(H[a]: "at least one mean differs")), cex = 2)
 
